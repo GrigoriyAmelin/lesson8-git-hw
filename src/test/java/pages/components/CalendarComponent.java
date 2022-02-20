7ebf95234a3521e558355268fd448ec1814d462a
amelin <g-amelin@mail.ru>
2022-02-20 22:56:28 +0300

    Changed CalendarComponent

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -9,6 +9,6 @@ public class CalendarComponent {
     public void setDate(String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--002").click();
+        $(".react-datepicker__day--002:not([react-datepicker__day--outside-month])").click();
     }
 }
