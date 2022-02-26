cf113600e1e04f24aa31a18d3fcd03026ef1fee5
amelin <g-amelin@mail.ru>
2022-02-27 01:46:04 +0300

    Modified SearchSelenideRepoTest

--- src/test/java/github/SearchSelenideRepoTest.java
+++ src/test/java/github/SearchSelenideRepoTest.java
@@ -1,7 +1,9 @@
 package github;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchSelenideRepoTest {
@@ -9,8 +11,10 @@ public class SearchSelenideRepoTest {
     @Test
     void shouldFindSelenideInGithub() {
 
-        open("https://github.com");
+        open("https://github.com/");
         $("[data-test-selector='nav-search-input']").setValue("selenide").pressEnter();
+        $("ul.repo-list li a").click();
+        $$("h1").first().shouldHave(text("selenide / selenide"));
 
         sleep(5000);
 
