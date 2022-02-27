f06e4f53aac0503de6bdc62f6b51527fd4673426
amelin <g-amelin@mail.ru>
2022-02-28 01:37:48 +0300

    Modified andreiSontsevShouldBeMainContractor

--- src/test/java/github/MainContributorTest.java
+++ src/test/java/github/MainContributorTest.java
@@ -1,17 +1,25 @@
 package github;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class MainContributorTest {
 
     @Test
     void andreiSontsevShouldBeMainContractor() {
+        Configuration.browserSize = "1020x1080";
         open("https://github.com/selenide/selenide");
-        $(".Layout-sidebar").$(withText("Contributors")).closest(".BorderGrid-cell");
+        $(".Layout-sidebar").$(withText("Contributors")).closest(".BorderGrid-row").scrollTo()
+                .$("ul  li").hover();
+        $$(".Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
+
+
+        sleep(3000);
     }
 }
