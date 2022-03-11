d316e590a88f5b7435a043c306e772fa072661e6
amelin <g-amelin@mail.ru>
2022-03-12 00:49:58 +0300

    Added A and B boxes checks

--- src/test/java/tests/DragAndDropTest.java
+++ src/test/java/tests/DragAndDropTest.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactTextCaseSensitive;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,14 +16,19 @@ public class DragAndDropTest {
 
         // Проверка открытия правильной страницы
         $(".example").shouldHave(text("Drag and Drop"));
+        sleep(1000);
+
+        // Проверка названий квадратов до их перемещения
+        $("#column-a").shouldHave(exactTextCaseSensitive("A"));
+        $("#column-b").shouldHave(exactTextCaseSensitive("B"));
 
         // Перенос элемента А на место элемента В
-        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(300,0)
-                .release().perform();
+        $("#column-a").dragAndDropTo($("#column-b"));
 
-//        actions().dragAndDropBy($("#column-a"), 200, 0).perform();
+        // Проверка названий квадратов после их перемещения
+        $("#column-a").shouldHave(exactTextCaseSensitive("B"));
+        $("#column-b").shouldHave(exactTextCaseSensitive("A"));
 
-        sleep(3000);
-
+        sleep(1000);
     }
 }
